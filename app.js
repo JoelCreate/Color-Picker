6f6ce468557fc2946b01dbb96bb24176469ef045
Joel Wiesner <j.wiesner88@gmail.com>
2023-07-13 18:47:30 -0700

    fetched Color API
    Fetched the Color API and then display 6 colors from color array data. I display this data with the actual color and hex value on the app. Styled in CSS

---
+++ app.js
@@ -0,0 +1,21 @@
+const colorScheme = document.getElementById("color-scheme")
+
+fetch("https://www.thecolorapi.com/scheme?hex=245657&mode=monochrome&count=6") 
+ .then(res => res.json())
+ .then(data => {
+
+    const colorArr = data.colors
+    console.log(colorArr)
+
+    let html =""
+
+    for (let colorInfo of colorArr) {
+        html += `
+        <div class="color-list" id="colors-displayed" style="background:${colorInfo.hex.value}"></div>
+        <div id="color-hex-values">${colorInfo.hex.value}</div>
+        `
+    }
+    
+    colorScheme.innerHTML = html
+
+ })
