6720e7f6a14e84e4881bb47ed640d4ae290fe2d0
Joel Wiesner <j.wiesner88@gmail.com>
2023-07-15 11:10:49 -0700

    created "color-selected" element from input type of color and added it to JS

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const colorScheme = document.getElementById("color-scheme")
+const colorSelected = document.getElementById("color-selected")
 
 fetch("https://www.thecolorapi.com/scheme?hex=245657&mode=monochrome&count=6") 
  .then(res => res.json())
