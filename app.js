b51952c2f1f6b74ef941bc3e293885adce3df3e2
Joel Wiesner <j.wiesner88@gmail.com>
2023-07-15 12:51:58 -0700

    converted selected color value into a query string and implemented into API fetch url to display colors
    Also cleaned up some coding

--- app.js
+++ app.js
@@ -1,32 +1,59 @@
 const colorScheme = document.getElementById("color-scheme")
-let colorSelected = document.getElementById("color-selected")
+const getColorSchemeBtn = document.getElementById("get-color-scheme")
+const selectScheme = document.getElementById("select-scheme")
+
+
+fetch(`https://www.thecolorapi.com/scheme?hex=000000&mode=monochrome`) 
+    .then(res => res.json())
+    .then(data => {
+
+        const colorArr = data.colors        
+        
+        let html =""
+
+        for (let colorInfo of colorArr) {
+            html += `
+            <div class="color-list" id="colors-displayed" style="background:${colorInfo.hex.value}"></div>
+            <!--<div id="color-hex-values">${colorInfo.hex.value}</div>-->
+            <div id="color-name-values">${colorInfo.name.value}</div>
+            `
+        }
+        
+        colorScheme.innerHTML = html
+
+})
+
+
+getColorSchemeBtn.addEventListener("click", function(){
+
+    let seedColor = document.getElementById("color-selected")
+    const selectedColor = seedColor.value
+    const queryString = 'hex=' + encodeURIComponent(selectedColor)    
+    const selectedColorScheme = selectScheme.value.toLowerCase()    
+
+    fetch(`https://www.thecolorapi.com/scheme?${queryString}&mode=${selectedColorScheme}`) 
+        .then(res => res.json())
+        .then(data => {
+
+            const colorArr = data.colors
+                        
+            let html =""
+
+            for (let colorInfo of colorArr) {
+                html += `
+                <div class="color-list" id="colors-displayed" style="background:${colorInfo.hex.value}"></div>
+                <!--<div id="color-hex-values">${colorInfo.hex.value}</div>-->
+                <div id="color-name-values">${colorInfo.name.value}</div>
+                `
+            }
+            
+            colorScheme.innerHTML = html
+
+    })
+
+})
 
 
 
-function monochrome() {
-    
-}
-
-monochrome()
 
 
-fetch("https://www.thecolorapi.com/scheme?hex=245657&mode=monochrome&count=6") 
- .then(res => res.json())
- .then(data => {
-
-    const colorArr = data.colors
-    console.log(colorArr)
-
-    let html =""
-
-    for (let colorInfo of colorArr) {
-        html += `
-        <div class="color-list" id="colors-displayed" style="background:${colorInfo.hex.value}"></div>
-        <div id="color-hex-values">${colorInfo.hex.value}</div>
-        <div id="color-name-values">${colorInfo.name.value}</div>
-        `
-    }
-    
-    colorScheme.innerHTML = html
-
- })
